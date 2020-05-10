5a8cc1d4e7c91934207f0ecf6690143360342125
mithunkumarc <mithunchannappa@gmail.com>
2020-05-10 09:56:34 +0530

    Create callback_function2.go

---
+++ functions/callback_function2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//callback function : sending function as argument
+	login_name := "vichitra" // success
+	// login_name := "" //failure
+	message := login(validation, login_name)
+	fmt.Println(message)
+}
+
+//this function is being sent as argument to square_calculator
+func validation(name string) bool {
+	if(len(name) > 0){
+		return true
+	}
+	return false
+}
+
+//first arugment is validating function
+//validation function signature must be same, arugments and return type
+func login(f func(name string) bool , name string) string {
+	t := f(name)
+	if(t){
+		return "success"
+	}
+	return "failure"
+}
