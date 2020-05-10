fcb51e8d83a9e8fdcebaa779b17d74bce0b926bf
mithunkumarc <mithunchannappa@gmail.com>
2020-05-10 09:04:55 +0530

    Create anonymous_func.go

---
+++ functions/anonymous_func.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+  //similar to self calling functions in javascript, python
+	func(x int) {
+		fmt.Println("anony",x)
+	}(42)	
+}
+
+//anony 42
