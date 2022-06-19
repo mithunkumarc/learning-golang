8659ab27f7b6e5db6db41f3788788c474fd65b31
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-06-19 06:23:32 +0000

    Add files via upload

---
+++ gorm/sqlite-example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	// db connection
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// Migrate : creating schema?
+	db.AutoMigrate((&Product{}))
+	// create
+	//db.Create(&Product{Code: "D42", Price: 100})
+	// Read
+
+	var product Product
+	db.First(&product, 1) // find product with primary key 1
+	fmt.Println(product)
+}
