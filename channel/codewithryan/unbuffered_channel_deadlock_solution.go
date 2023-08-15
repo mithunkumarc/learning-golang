06698a19139feb7c7790d6bce894a76c91bd4c53
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2023-08-15 12:42:16 +0530

    Create unbuffered_channel_deadlock_solution.go

---
+++ channel/codewithryan/unbuffered_channel_deadlock_solution.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// creating channel int in a separate goroutine
+	dataChan := make(chan int) // unbuffered channel(no memory)
+	go func() {
+		dataChan <- 789
+	}()
+	n := <-dataChan
+	fmt.Println(n)
+}
+
+/*
+	output : 789
+	unbuffered deadlock solution :
+	in one goroutine(thread) : send data
+	and in another goroutine(thread) receive data
+*/
