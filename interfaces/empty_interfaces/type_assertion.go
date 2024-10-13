127a7afbdcab916d8fcc7ba3e5684fe8a13aac13
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2024-10-13 16:50:34 +0530

    Update type_assertion.go

--- interfaces/empty_interfaces/type_assertion.go
+++ interfaces/empty_interfaces/type_assertion.go
@@ -1,1 +1,23 @@
 
+/*
+  A type assertion provides access to an interface value's underlying concrete value.
+  t := i.(T)
+*/
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello" // emtpy interface can hold any type value
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
+}
